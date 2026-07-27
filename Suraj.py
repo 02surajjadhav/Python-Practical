a4f6fa2fb29f098348e768e0db1ad51faa019df4
Suraj Jadhav <sj2827684@gmail.com>
2026-07-27 13:26:07 +0530

    Add files via upload

--- Suraj.py
+++ Suraj.py
@@ -1,26 +1,9 @@
-print("..........Admmission Eligibility Check..........")
-age=int(input("Enter Age Of Student:"))
-marks=int(input("Enter Marks:"))
+count= 0
+while count<5:
+    if count ==3:
+        count +=1
+        continue
+    print(count)
+    count +=1
 
-if(age>=17 and age<=25):
-    print("Eligible for Admission by Age")
 
-    if(marks>60):
-        print("Eligible for B.Tech")
-
-        if(marks>85):
-            print("Eligible for AIML")
-        
-        elif(marks>75):
-            print("Eligible For CSE")
-        
-        else:
-            print("Eligible for MECH,ENTC,CIVIL,ELECTRICAL")
-    
-    else:
-        print("Not Eligible for B.Tech")
-
-else:
-    print("Not Eligible for Admission by Age")
-
-print("........🙏🙏Thank you🙏🙏..........")
