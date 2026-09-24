e3eca4871362c1603562ed653979e9679b282bdc
Suraj Jadhav <sj2827684@gmail.com>
2026-09-24 15:58:07 +0530

    Update Practical no.12.py

--- Practical no.12.py
+++ Practical no.12.py
@@ -1,8 +1,8 @@
 locations = [
-    ("Pune", 18.5204, 73.8567),
-    ("Mumbai", 19.0760, 72.8777),
-    ("Nashik", 19.9975, 73.7898),
-    ("Hyderabad", 17.3850, 78.4867)
+    ("Palus", 18.5204, 73.8567),
+    ("Vita", 19.0760, 72.8777),
+    ("Karve", 19.9975, 73.7898),
+    ("Sangli", 17.3850, 78.4867)
 ]
 
 print("1. Original Locations:")
@@ -16,6 +16,7 @@ print("Name:", first_location[0])
 print("Latitude:", first_location[1])
 print("Longitude:", first_location[2])
 
+
 print("\n3. Tuple Unpacking:")
 
 name, latitude, longitude = first_location
@@ -24,27 +25,7 @@ print("Name:", name)
 print("Latitude:", latitude)
 print("Longitude:", longitude)
 
-print("\n4. Sorted by Latitude:")
-
-sorted_by_latitude = sorted(
-    locations,
-    key=lambda location: location[1]
-)
-
-for name, latitude, longitude in sorted_by_latitude:
-    print(f"{name}: ({latitude}, {longitude})")
-
-print("\n5. Sorted by Longitude:")
-
-sorted_by_longitude = sorted(
-    locations,
-    key=lambda location: location[2]
-)
-
-for name, latitude, longitude in sorted_by_longitude:
-    print(f"{name}: ({latitude}, {longitude})")
-
-print("\n6. Extracted Coordinate Tuples:")
+print("\n4. Extracted Coordinate Tuples:")
 
 coordinates = []
 
@@ -54,3 +35,23 @@ for name, latitude, longitude in locations:
 
 for latitude, longitude in coordinates:
     print(f"Latitude = {latitude}, Longitude = {longitude}")
+    
+print("\n5. Sorted by Latitude:")
+
+sorted_by_latitude = sorted(
+    locations,
+    key=lambda location: location[1]
+)
+
+for name, latitude, longitude in sorted_by_latitude:
+    print(f"{name}: ({latitude}, {longitude})")
+
+print("\n6. Sorted by Longitude:")
+
+sorted_by_longitude = sorted(
+    locations,
+    key=lambda location: location[2]
+)
+
+for name, latitude, longitude in sorted_by_longitude:
+    print(f"{name}: ({latitude}, {longitude})")
